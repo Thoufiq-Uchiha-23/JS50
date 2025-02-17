c540ba150e81215ae4c67438679b4408a00b21da
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-02-17 22:17:12 +0530

    question 3

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // question 1
-console.log("thoufiq", "teach how to code")
+// console.log("thoufiq", "teach how to code")
 
 // question 2
-console.log(45*2 - 10)  // OUTPUT: 80
+// console.log(45*2 - 10)  // OUTPUT: 80
+
+// question 3
+const newdate = new Date()
+console.log(newdate.getFullYear())
