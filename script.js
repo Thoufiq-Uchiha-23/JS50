ff483c35e2fa8fa5fd75d66d520ef701699fc682
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-02-17 22:19:04 +0530

    question 4

--- script.js
+++ script.js
@@ -5,5 +5,12 @@
 // console.log(45*2 - 10)  // OUTPUT: 80
 
 // question 3
-const newdate = new Date()
-console.log(newdate.getFullYear())
+// const newdate = new Date()
+// console.log(newdate.getFullYear())
+
+// question 4
+let first = "Thoufiq";
+let last = "Izhar";
+
+console.log(first + " " + last)
+console.log(`${first} ${last}`)
