e6b0c1c1c58f1528a44d75d0199e28a9a4ad0f98
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-03-02 19:48:48 +0530

    q28

--- script.js
+++ script.js
@@ -141,3 +141,10 @@
 //    7 8 9
 
 // question 28
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (let i = 0; i < Math.floor(arr.length / 2); i++) {
+  var temp = arr[i];
+  arr[i] = arr[arr.length - 1 - i];
+  arr[arr.length - 1 - i] = temp;
+}
+console.log(arr); //OP: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
